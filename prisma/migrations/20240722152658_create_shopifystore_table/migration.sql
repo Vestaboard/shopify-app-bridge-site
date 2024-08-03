8db9a21c800784d54248e4b9f57d390dc3469515
Sully Syed <sully@yllus.com>
2024-08-03 15:57:17 -0400

    Database: Drop the ShopifyStore table since we'll just sync off the Laravel database.

--- prisma/migrations/20240722152658_create_shopifystore_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `ShopifyStore` (
-    `id` VARCHAR(191) NOT NULL,
-    `shop` VARCHAR(191) NOT NULL,
-    `authToken` VARCHAR(191) NOT NULL,
-    `isAuthorized` BOOLEAN NULL DEFAULT false,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/migrations/20240730204140_remove_authtoken_from_shopifystore_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `authToken` on the `ShopifyStore` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `ShopifyStore` DROP COLUMN `authToken`;
