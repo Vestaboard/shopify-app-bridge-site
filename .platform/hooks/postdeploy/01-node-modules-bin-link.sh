31581f2d9b502874c7b4e7262bcbe4df61ff7e22
Sully Syed <sully@yllus.com>
2025-06-04 01:06:00 +0900

    Config: Add 01-remix-vite-build.sh to build the RemixJS app; add 01-node-modules-bin-link.sh to symlink required binaries to PATH.

---
+++ .platform/hooks/postdeploy/01-node-modules-bin-link.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -nfs /var/app/current/node_modules/.bin/prisma /usr/local/bin/prisma
+ln -nfs /var/app/current/node_modules/.bin/remix /usr/local/bin/remix
+ln -nfs /var/app/current/node_modules/.bin/remix-serve /usr/local/bin/remix-serve
+ln -nfs /var/app/current/node_modules/.bin/shopify /usr/local/bin/shopify
+ln -nfs /var/app/current/node_modules/.bin/vite /usr/local/bin/vite
